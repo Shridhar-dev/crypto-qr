2d02807b2bb884d37976a14e999666b613641408
Shridhar-dev <shridharkamat10@gmail.com>
2022-03-27 17:34:07 +0530

    minor bug fixes

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
     <div className='font-sans min-h-screen w-full text-white flex justify-center items-center flex-col '>
         <img src={Logo} width="200" />
         <h1 className=' text-5xl mb-8'>CryptoQR</h1>
-        <span className=' text-2xl'>An easy way to send crypto transactions using QR</span>
+        <span className=' text-2xl text-center px-10'>An easy way to send crypto transactions using QR</span>
         <Link to="/qrscan">
           <button className=' bg-white text-black p-2 px-5 mt-5'> Pay Now </button>
         </Link>
